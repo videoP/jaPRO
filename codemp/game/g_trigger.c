53aed964d6f5e4b74fb6ad6d0dfc9ba603b6bd57
videoP <videoprofess@gmail.com>
2022-09-18 19:45:26 -0400

    one way push improvement?

--- codemp/game/g_trigger.c
+++ codemp/game/g_trigger.c
@@ -1963,17 +1963,17 @@ void NewPush(gentity_t *trigger, gentity_t *player, trace_t *trace) {//JAPRO Tim
 	}
 
 	if (trigger && trigger->spawnflags & 32) { //Spawnflags 4 deadstops them if they are traveling in this direction... sad hack to let people retroactively fix maps without barriers 
-		if (trigger->speed == 0 && player->client->ps.velocity[0] > 0) {
-			player->client->ps.velocity[0] = player->client->ps.velocity[1] = 0;
+		if (trigger->speed == 0 && player->client->ps.velocity[0] > player->client->ps.speed+20) {
+			player->client->ps.velocity[0] = player->client->ps.speed;
 		}
-		else if (trigger->speed == 90 && player->client->ps.velocity[1] > 0) {
-			player->client->ps.velocity[0] = player->client->ps.velocity[1] = 0;
+		else if (trigger->speed == 90 && player->client->ps.velocity[1] > player->client->ps.speed+20) {
+			player->client->ps.velocity[1] = player->client->ps.speed;
 		}
-		else if (trigger->speed == 180 && player->client->ps.velocity[0] < 0) {
-			player->client->ps.velocity[0] = player->client->ps.velocity[1] = 0;
+		else if (trigger->speed == 180 && player->client->ps.velocity[0] < -player->client->ps.speed-20) {
+			player->client->ps.velocity[0] = -player->client->ps.speed;
 		}
-		else if (trigger->speed == 270 && player->client->ps.velocity[1] < 0) {
-			player->client->ps.velocity[0] = player->client->ps.velocity[1] = 0;
+		else if (trigger->speed == 270 && player->client->ps.velocity[1] < -player->client->ps.speed-20) {
+			player->client->ps.velocity[1] = -player->client->ps.speed;
 		}
 		return;
 	}
